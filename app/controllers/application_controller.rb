68725be67533c67c96af7e2e3211859cf3e927ae
Dave Strus <dave@getfretless.com>
2016-06-08 14:11:37 -0400

    CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+
+  protected
+
+  # def authorize_api_key
+  #   unless current_api_user.present?
+  #     render nothing: true, status: :unauthorized
+  #   end
+  # end
+  #
+  # private
+  #
+  # def current_api_user
+  #   @current_api_user ||= User.find_by(api_key: params[:api_key]) if params[:api_key]
+  # end
 end
