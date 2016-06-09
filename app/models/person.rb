d9f6beaa9e59a8c516789a7128fbd62357659575
Dave Strus <dave@getfretless.com>
2016-06-09 13:21:39 -0400

    Destroy dependent rivalries when destroying a person.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
 class Person < ActiveRecord::Base
-  has_many :rivalries
+  has_many :rivalries, dependent: :destroy
   has_many :rivals, through: :rivalries, table_name: 'people'
+  has_many :perceived_rivalries, class_name: 'Rivalry', foreign_key: 'rival_id', dependent: :destroy
+  has_many :haters, through: :rivalries, class_name: 'Person', foreign_key: 'person_id'
 
   validates :name, presence: true
 end
